db61639a93fce605912f6c2dc27fa1cde8b2fd4a
matheushfp <mathzhf@gmail.com>
2023-10-18 12:23:12 -0300

    add integration tests

--- ignite/nodejs/trilha-2022/rentx/src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
+++ ignite/nodejs/trilha-2022/rentx/src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
@@ -1,7 +1,7 @@
 import { inject, injectable } from "tsyringe";
 
-import { AppError } from "@shared/errors/AppError";
 import { ICategoriesRepository } from "@modules/cars/repositories/ICategoriesRepository";
+import { AppError } from "@shared/errors/AppError";
 
 interface IRequest {
   name: string;
@@ -23,7 +23,7 @@ class CreateCategoryUseCase {
       throw new AppError("Category already exists!");
     }
 
-    this.categoriesRepository.create({ name, description });
+    await this.categoriesRepository.create({ name, description });
   }
 }
 
---
+++ ignite/nodejs/trilha-2022/rentx/src/modules/cars/useCases/listCategories/ListCategoriesController.spec.ts
@@ -0,0 +1,52 @@
+import { hash } from "bcryptjs";
+import request from "supertest";
+import { Connection, createConnection } from "typeorm";
+import { v4 as uuidV4 } from "uuid";
+
+import { app } from "@shared/infra/http/app";
+
+let connection: Connection;
+
+describe("List Categories Controller", () => {
+  beforeAll(async () => {
+    connection = await createConnection();
+    await connection.runMigrations();
+
+    const id = uuidV4();
+    const password = await hash("admin", 8);
+
+    await connection.query(
+      `INSERT INTO USERS (id, name, email, password, driver_license, is_admin, created_at)
+          values ('${id}', 'admin', 'admin@rentx.com', '${password}', 'XXXXXXXX', true, 'now()')`,
+    );
+  });
+
+  afterAll(async () => {
+    await connection.dropDatabase();
+    await connection.close();
+  });
+
+  it("Should be able to list all categories", async () => {
+    const responseToken = await request(app).post("/sessions").send({
+      email: "admin@rentx.com",
+      password: "admin",
+    });
+
+    const { token } = responseToken.body;
+
+    await request(app)
+      .post("/categories")
+      .send({
+        name: "CategoryTest",
+        description: "CategoryDescriptionTest",
+      })
+      .set("Authorization", `Bearer ${token}`);
+
+    const response = await request(app).get("/categories");
+
+    expect(response.status).toBe(200);
+    expect(response.body.length).toBe(1);
+    expect(response.body[0]).toHaveProperty("id");
+    expect(response.body[0].name).toEqual("CategoryTest");
+  });
+});
