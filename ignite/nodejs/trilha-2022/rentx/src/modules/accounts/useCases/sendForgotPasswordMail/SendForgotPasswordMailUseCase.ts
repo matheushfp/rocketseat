912986dcbc0a9978bfd8639fce2d6fdc31221ca1
matheushfp <mathzhf@gmail.com>
2023-11-04 16:48:29 -0300

    add SESMailProvider + .env example

--- ignite/nodejs/trilha-2022/rentx/src/modules/accounts/useCases/sendForgotPasswordMail/SendForgotPasswordMailUseCase.ts
+++ ignite/nodejs/trilha-2022/rentx/src/modules/accounts/useCases/sendForgotPasswordMail/SendForgotPasswordMailUseCase.ts
@@ -20,7 +20,7 @@ class SendForgotPasswordMailUseCase {
     @inject("DayJSDateProvider")
     private dateProvider: IDateProvider,
 
-    @inject("EtherealMailProvider")
+    @inject("MailProvider")
     private mailProvider: IMailProvider,
   ) {}
 
--- ignite/nodejs/trilha-2022/rentx/src/shared/container/providers/index.ts
+++ ignite/nodejs/trilha-2022/rentx/src/shared/container/providers/index.ts
@@ -4,6 +4,7 @@ import { IDateProvider } from "./DateProvider/IDateProvider";
 import { DayJSDateProvider } from "./DateProvider/implementations/DayJSDateProvider";
 import { IMailProvider } from "./MailProvider/IMailProvider";
 import { EtherealMailProvider } from "./MailProvider/implementations/EtherealMailProvider";
+import { SESMailProvider } from "./MailProvider/implementations/SESMailProvider";
 import { LocalStorageProvider } from "./StorageProvider/implementations/LocalStorageProvider";
 import { S3StorageProvider } from "./StorageProvider/implementations/S3StorageProvider";
 import { IStorageProvider } from "./StorageProvider/IStorageProvider";
@@ -13,9 +14,14 @@ container.registerSingleton<IDateProvider>(
   DayJSDateProvider,
 );
 
+const mailProvider = {
+  ethereal: container.resolve(EtherealMailProvider),
+  ses: container.resolve(SESMailProvider),
+};
+
 container.registerInstance<IMailProvider>(
-  "EtherealMailProvider",
-  new EtherealMailProvider(),
+  "MailProvider",
+  mailProvider[process.env.MAIL_PROVIDER],
 );
 
 const diskStorage = {
