e707cd5f984f3cee4597ce3d9c8d4ed8107c1461
matheushfp <mathzhf@gmail.com>
2023-11-07 13:35:29 -0300

    add react folder + initial ignite feed project

---
+++ ignite/reactjs/ignite-feed/src/App.jsx
@@ -0,0 +1,33 @@
+import { Post } from "./Post"
+import { Header } from "./components/Header"
+
+
+import styles from "./App.module.css"
+import "./global.css"
+import { Sidebar } from "./components/Sidebar"
+
+export function App() {
+
+  return (
+    <div>
+      <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="Matheus"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo id facere possimus voluptatibus aspernatur necessitatibus architecto, iure vel tempora soluta eligendi ea quo nesciunt, sint molestias sit quos, optio molestiae."
+          />
+
+          <Post
+            author="Teste"
+            content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit quaerat vitae voluptatem voluptatum nihil nostrum dolorem, harum quibusdam praesentium aspernatur."
+          />
+        </main>
+      </div>
+
+    </div>
+  )
+}
+
---
+++ ignite/reactjs/ignite-feed/src/components/Sidebar.jsx
@@ -0,0 +1,28 @@
+import { PencilLine } from "@phosphor-icons/react"
+
+import styles from "./Sidebar.module.css"
+
+export function Sidebar() {
+    return (
+        <aside className={styles.sidebar}>
+            <img
+                className={styles.cover}
+                src="https://images.unsplash.com/photo-1695219770898-c8aa56fb111a?q=50&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            />
+
+            <div className={styles.profile}>
+                <img className={styles.avatar} src="https://github.com/matheushfp.png" />
+                <strong>Matheus Ferreira</strong>
+                <span>Web Developer</span>
+            </div>
+
+            <footer>
+                <a href="#">
+                    <PencilLine size={20} />
+                    Editar seu perfil
+                </a>
+            </footer>
+        </aside>
+
+    );
+}
