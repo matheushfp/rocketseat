335eecd09c898d1e2c8137bd40264676f0ee407c
matheushfp <mathzhf@gmail.com>
2024-06-01 20:29:33 -0300

    add schedule page + calendar component

--- ignite/reactjs/ignite-call/src/pages/register/time-intervals/index.page.tsx
+++ ignite/reactjs/ignite-call/src/pages/register/time-intervals/index.page.tsx
@@ -22,6 +22,7 @@ import { getWeekDays } from '@/utils/get-week-days'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { convertTimeStringToMinutes } from '@/utils/convert-time-string-to-minutes'
 import { api } from '@/lib/axios'
+import { useRouter } from 'next/router'
 
 const timeIntervalsFormSchema = z.object({
   intervals: z
@@ -84,6 +85,8 @@ export default function TimeIntervals() {
     },
   })
 
+  const router = useRouter()
+
   const { fields } = useFieldArray({
     name: 'intervals',
     control,
@@ -99,6 +102,8 @@ export default function TimeIntervals() {
     await api.post('/users/time-intervals', {
       intervals,
     })
+
+    await router.push('/register/update-profile')
   }
 
   return (
--- ignite/reactjs/ignite-call/src/utils/get-week-days.ts
+++ ignite/reactjs/ignite-call/src/utils/get-week-days.ts
@@ -1,9 +1,19 @@
-export function getWeekDays() {
+interface GetWeekDaysParams {
+  short?: boolean
+}
+
+export function getWeekDays({ short = false }: GetWeekDaysParams = {}) {
   const formatter = new Intl.DateTimeFormat('pt-BR', {
     weekday: 'long',
   })
 
   return Array.from(Array(7).keys())
     .map((day) => formatter.format(new Date(Date.UTC(2021, 5, day))))
-    .map((weekDay) => weekDay.charAt(0).toUpperCase().concat(weekDay.slice(1)))
+    .map((weekDay) => {
+      if (short) {
+        return weekDay.substring(0, 3).toUpperCase()
+      }
+
+      return weekDay.charAt(0).toUpperCase().concat(weekDay.slice(1))
+    })
 }
