a7410f8ce7a865b3466c8bc195378f66856beefe
matheushfp <mathzhf@gmail.com>
2024-08-30 04:06:15 -0300

    add login page and toast notifications

--- ignite/reactjs/pizza-shop/src/App.tsx
+++ ignite/reactjs/pizza-shop/src/App.tsx
@@ -1,13 +1,16 @@
-import { RouterProvider } from 'react-router-dom'
-
 import './global.css'
-import { router } from './routes'
+
 import { Helmet, HelmetProvider } from 'react-helmet-async'
+import { RouterProvider } from 'react-router-dom'
+import { Toaster } from 'sonner'
+
+import { router } from './routes'
 
 export function App() {
   return (
     <HelmetProvider>
-      <Helmet titleTemplate='%s | pizza.shop' />
+      <Helmet titleTemplate="%s | pizza.shop" />
+      <Toaster richColors />
       <RouterProvider router={router} />
     </HelmetProvider>
   )
