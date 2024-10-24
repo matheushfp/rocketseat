5e3bc98b9f8b92117a3dd4fd76e386905dcd4a8b
matheushfp <mathzhf@gmail.com>
2024-10-24 15:29:38 -0300

    add put/patch routes in meals

--- ignite/nodejs/trilha-atualizada/daily-diet-api/src/routes/meals.ts
+++ ignite/nodejs/trilha-atualizada/daily-diet-api/src/routes/meals.ts
@@ -67,12 +67,20 @@ export async function mealsRoutes(app: FastifyInstance) {
     const { id } = params.data
     const { sub: userId } = request.user
 
-    const meal = await prisma.meal.findFirst({
+    const meal = await prisma.meal.findUnique({
       where: {
         id,
-        user_id: userId,
       },
     })
+
+    if(!meal) {
+      return reply.status(404).send()
+    }
+
+    // Return Unauthorized if the user didn't create the meal
+    if(meal.user_id !== userId) {
+      return reply.status(401).send()
+    }
   
     return {
       meal,
@@ -96,20 +104,156 @@ export async function mealsRoutes(app: FastifyInstance) {
     const { id } = params.data
     const { sub: userId } = request.user
 
-    try{
-      await prisma.meal.delete({
-        where: {
-          id,
-          user_id: userId,
-        },
-      })
-    } catch(err) {
-      console.error(err)
-      return reply.status(404).send({
-        message: 'Meal Not Found',
+    const meal = await prisma.meal.findUnique({
+      where: {
+        id,
+      },
+    })
+
+    if(!meal) {
+      return reply.status(404).send()
+    }
+
+    if(meal.user_id !== userId) {
+      return reply.status(401).send()
+    }
+
+    await prisma.meal.delete({
+      where: {
+        id,
+        user_id: userId,
+      },
+    })
+    
+    return reply.status(204).send()
+  })
+
+  app.put('/:id', async (request, reply) => {
+    const paramsSchema = z.object({
+      id: z.string().uuid(),
+    })
+      
+    const params = paramsSchema.safeParse(request.params)
+      
+    if(!params.success) {
+      return reply.status(400).send({
+        message: params.error.format(),
       })
     }
-    
+      
+    const { id } = params.data
+    const { sub: userId } = request.user
+
+    const meal = await prisma.meal.findUnique({
+      where: {
+        id,
+      },
+    })
+
+    if(!meal) {
+      return reply.status(404).send()
+    }
+
+    if(meal.user_id !== userId) {
+      return reply.status(401).send()
+    }
+
+    const updateMealBodySchema = z.object({
+      name: z.string(),
+      description: z.string(),
+      date: z.string().datetime(),
+      is_on_diet: z.boolean(),
+    })
+
+    const body = updateMealBodySchema.safeParse(request.body)
+
+    if(!body.success) {
+      return reply.status(400).send({
+        message: body.error.format(),
+      })
+    }
+
+    const { data } = body
+
+    await prisma.meal.update({
+      where: {
+        id,
+        user_id: userId,
+      },
+      data: {
+        ...data,
+        updated_at: new Date(),
+      },
+    })
+
+    return reply.status(204).send()
+  })
+
+  app.patch('/:id', async (request, reply) => {
+    const paramsSchema = z.object({
+      id: z.string().uuid(),
+    })
+      
+    const params = paramsSchema.safeParse(request.params)
+      
+    if(!params.success) {
+      return reply.status(400).send({
+        message: params.error.format(),
+      })
+    }
+      
+    const { id } = params.data
+    const { sub: userId } = request.user
+
+    const meal = await prisma.meal.findUnique({
+      where: {
+        id,
+      },
+    })
+
+    if(!meal) {
+      return reply.status(404).send()
+    }
+
+    if(meal.user_id !== userId) {
+      return reply.status(401).send()
+    }
+
+    const updateMealBodySchema = z.object({
+      name: z.string().optional(),
+      description: z.string().optional(),
+      date: z.string().datetime().optional(),
+      is_on_diet: z.boolean().optional(),
+    })
+
+    const body = updateMealBodySchema.safeParse(request.body)
+
+    if(!body.success) {
+      return reply.status(400).send({
+        message: body.error.format(),
+      })
+    }
+
+    const { data } = body
+
+    if(Object.keys(data).length === 0) {
+      return reply.status(400).send({
+        message: 
+        'At least one should be informed: name, description, date, is_on_diet',
+      })
+    }
+
+    await prisma.meal.update({
+      where: {
+        id,
+        user_id: userId,
+      },
+      data: {
+        ...data,
+        updated_at: new Date(),
+      },
+    })
+
     return reply.status(204).send()
   })
 }
